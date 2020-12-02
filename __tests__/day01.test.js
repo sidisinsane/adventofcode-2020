97e6d6377dda8a611aa3be2ab96e619b141afdca
Dirk Sidney Jansen <sid@sidisinsane.com>
2020-12-02 18:38:52 +0100

    fix: fixed typo

--- __tests__/day01.test.js
+++ __tests__/day01.test.js
@@ -3,6 +3,6 @@ const { expect } = require('@jest/globals');
 const twoSumArrayProduct = require('../puzzles/day01/index.cjs');
 const data = require('../puzzles/day01/data.json');
 
-test('finds the two entries that sum to 2020 and then multiply those two numbers to equal 646779', () => {
+test('finds the two entries that sum to 2020 and then multiplies those two numbers to equal 646779', () => {
   expect(twoSumArrayProduct(data, 2020)).toBe(646779);
 });
